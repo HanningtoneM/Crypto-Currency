c64aed518a08fa90b8ee95e2b6b80c724f639d79
HanningtoneM <hanningtonepompyy@gmail.com>
2022-07-25 12:55:12 +0300

    Add Detail.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,2 @@
 
-import NavBar from './Components/NavBar';
-
-
-
-function App() {
-  const [page, setPage] = useState('home');
-  return (
-
-        <NavBar page={page} set={setPage} />
-  );
-}
-
-export default App;
 
