2362c07415217495b995cfa3602aaca5ed3d500a
HanningtoneM <hanningtonepompyy@gmail.com>
2022-07-25 12:44:23 +0300

    Add navbar.js in the components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import NavBar from './Components/NavBar';
+
+
 
 function App() {
+  const [page, setPage] = useState('home');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <NavBar page={page} set={setPage} />
   );
 }
 
 export default App;
+
